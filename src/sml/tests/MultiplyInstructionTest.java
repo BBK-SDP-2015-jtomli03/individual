9c6207e9e64dce5383f7388e6df1ec104706be38
jtomli03 <jotom1000@gmail.com>
2015-02-07 00:15:22 +0000

    MultiplyInstructionTest class added
    with method testExecute()

---
+++ src/sml/tests/MultiplyInstructionTest.java
@@ -0,0 +1,35 @@
+package sml.tests;
+
+import org.junit.Before;
+import org.junit.Test;
+import sml.Machine;
+import sml.MultiplyInstruction;
+import sml.Registers;
+
+import static org.junit.Assert.assertEquals;
+
+public class MultiplyInstructionTest {
+
+    private MultiplyInstruction ins;
+    private Machine m;
+
+    @Before
+    public void setUp() throws Exception {
+        ins = new MultiplyInstruction("f1", 1, 2, 3);
+        m = new Machine();
+        m.setRegisters(new Registers());
+        m.getRegisters().setRegister(2, 10);
+        m.getRegisters().setRegister(3, 5);
+    }
+
+    @Test
+    public void testExecute() throws Exception {
+        ins.execute(m); // result of 10 * 5 into register 1
+        assertEquals(50, m.getRegisters().getRegister(1));
+    }
+
+    @Test
+    public void testToString() throws Exception {
+
+    }
+}
