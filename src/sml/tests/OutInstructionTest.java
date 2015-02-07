e08e745ed10e1fbac61e66868e1d00b7b24f4d51
jtomli03 <jotom1000@gmail.com>
2015-02-07 01:22:09 +0000

    OutInstructionTest class added
    with methods testExecute(), and testToString()

---
+++ src/sml/tests/OutInstructionTest.java
@@ -0,0 +1,45 @@
+package sml.tests;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import sml.Machine;
+import sml.OutInstruction;
+import sml.Registers;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.Assert.assertEquals;
+
+public class OutInstructionTest {
+
+    private OutInstruction ins;
+    private Machine m;
+    private final ByteArrayOutputStream output = new ByteArrayOutputStream();
+
+    @Before
+    public void setUp() throws Exception {
+        System.setOut(new PrintStream(output));
+        ins = new OutInstruction("f1", 1);
+        m = new Machine();
+        m.setRegisters(new Registers());
+        m.getRegisters().setRegister(1, 10);
+    }
+
+    @After
+    public void cleanUp() throws Exception{
+        System.setOut(null);
+    }
+
+    @Test
+    public void testExecute() throws Exception {
+        ins.execute(m);
+        assertEquals("10\n", output.toString());
+    }
+
+    @Test
+    public void testToString() throws Exception {
+        assertEquals("f1: out register 1", ins.toString());
+    }
+}
