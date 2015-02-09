163484a40d3b69c2718793029bcd20c9c7b2ae54
jtomli03 <jotom1000@gmail.com>
2015-02-09 15:22:19 +0000

    MultiplyInstructionTest updated
    addition of methods to test if an ArrayIndexOutOfBoundsException is
    thrown if any of the register parameters do not exist

--- src/sml/tests/MultiplyInstructionTest.java
+++ src/sml/tests/MultiplyInstructionTest.java
@@ -11,6 +11,7 @@ import static org.junit.Assert.assertEquals;
 public class MultiplyInstructionTest {
 
     private MultiplyInstruction ins;
+    private MultiplyInstruction ins2;
     private Machine m;
 
     @Before
@@ -26,6 +27,24 @@ public class MultiplyInstructionTest {
     public void testExecute() throws Exception {
         ins.execute(m); // result of 10 * 5 into register 1
         assertEquals(50, m.getRegisters().getRegister(1));
+    }
+
+    @Test(expected = ArrayIndexOutOfBoundsException.class)
+    public void testExecuteThrowsExceptionIfResultRegisterParameterNotInRange(){
+        ins2 = new MultiplyInstruction("f1", 33, 2, 3); //register 33 doesn't exist
+        ins2.execute(m);
+    }
+
+    @Test(expected = ArrayIndexOutOfBoundsException.class)
+    public void testExecuteThrowsExceptionIfOp1RegisterParameterNotInRange(){
+        ins2 = new MultiplyInstruction("f1", 2, 33, 3); //register 33 doesn't exist
+        ins2.execute(m);
+    }
+
+    @Test(expected = ArrayIndexOutOfBoundsException.class)
+    public void testExecuteThrowsExceptionIfOp2ResultRegisterParameterNotInRange(){
+        ins2 = new MultiplyInstruction("f1", 2, 2, 33); //register 33 doesn't exist
+        ins2.execute(m);
     }
 
     @Test
